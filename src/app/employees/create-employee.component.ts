2a2ba3f02d9069fa1ef7370de25bd1027bb80671
hetjoshi87 <hetjoshi87@gmail.com>
2019-10-31 15:07:14 -0500

    validating error email

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -17,7 +17,7 @@ export class CreateEmployeeComponent implements OnInit {
     id: null,
     name:  null,
     gender:  null,
-    email:  null,
+    email:  '',
     phoneNumber:  null,
     contactPreference:  null,
     dateOfBirth:  null,
